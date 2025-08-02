d7dc84a800483137d394607563aee88e59e9e032
anime-song <52844453+anime-song@users.noreply.github.com>
2025-08-02 19:56:36 +0900

    Update transcribe.py

--- transkun/transcribe.py
+++ transkun/transcribe.py
@@ -27,12 +27,8 @@ def main():
     argumentParser = argparse.ArgumentParser()
     argumentParser.add_argument("audioPath", help="path to the input audio file")
     argumentParser.add_argument("outPath", help="path to the output MIDI file")
-    argumentParser.add_argument(
-        "--weight", default=defaultWeight, help="path to the pretrained weight"
-    )
-    argumentParser.add_argument(
-        "--conf", default=defaultConf, help="path to the model conf"
-    )
+    argumentParser.add_argument("--weight", default=defaultWeight, help="path to the pretrained weight")
+    argumentParser.add_argument("--conf", default=defaultConf, help="path to the model conf")
     argumentParser.add_argument(
         "--device",
         default="cpu",
@@ -55,7 +51,6 @@ def main():
         "--use_state_dict",
         action="store_true",
     )
-    argumentParser.add_argument("--audio_save_path", type=str, default=None)
 
     args = argumentParser.parse_args()
 
@@ -74,11 +69,7 @@ def main():
     model: TransKun = transkun(conf=conf).to(device)
     model = torch.compile(model, mode="reduce-overhead", fullgraph=True)
 
-    if (
-        "best_state_dict" not in checkpoint
-        or checkpoint["best_state_dict"] is None
-        or args.use_state_dict
-    ):
+    if "best_state_dict" not in checkpoint or checkpoint["best_state_dict"] is None or args.use_state_dict:
         model.load_state_dict(checkpoint["state_dict"], strict=False)
     else:
         model.load_state_dict(checkpoint["best_state_dict"], strict=False)
@@ -94,7 +85,7 @@ def main():
 
     x = torch.from_numpy(audio).to(device)
 
-    notesEst, recon_audio = model.transcribe(
+    notesEst = model.transcribe(
         x,
         stepInSecond=args.segmentHopSize,
         segmentSizeInSecond=args.segmentSize,
@@ -103,22 +94,6 @@ def main():
 
     outputMidi = writeMidi(notesEst)
     outputMidi.write(outPath)
-
-    stem_name_list = ["piano", "other"]
-    audio_save_path = args.audio_save_path
-    if audio_save_path is None:
-        audio_save_path = Path(audioPath).parent
-    else:
-        audio_save_path = Path(audio_save_path)
-    audio_file_name = Path(audioPath).stem
-    audio_save_path.mkdir(parents=True, exist_ok=True)
-
-    for i in range(recon_audio.shape[0]):
-        # recon_audio: (C, N, T)
-        recon_np = recon_audio[:, i].transpose(0, 1).cpu().numpy()  # (T, C)
-        write_wav_path = audio_save_path / f"{audio_file_name}_{stem_name_list[i]}.wav"
-
-        sf.write(write_wav_path, recon_np, conf.fs, subtype="PCM_16")
 
 
 if __name__ == "__main__":
